4ad66c7751312de77ffef8a48f1be4cc5c957222
andrew <andrew12ksh@gmail.com>
2021-07-02 10:23:18 +0900

    dev test2

---
+++ test2.tsx
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+const Test2 = (props: Props) => {
+    return (
+        <div>
+
+            </div>
+    );
+};
+
+export default Test2
